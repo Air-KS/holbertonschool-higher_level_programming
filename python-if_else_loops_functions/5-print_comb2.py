7fa533d95bcec77ba8eb8f3d304551f96e3d9609
Air-KS <kevinrogeret@gmail.com>
2023-05-29 14:00:56 +0000

    Write a program that prints numbers from 00 to 99

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -4,6 +4,6 @@
 """Write a program that prints numbers from 00 to 99"""
 for number in range(0, 100):
     if number == 99:
-        print(f"{number}")
+        print("{}".format(number))
     else:
         print("{:02}, ".format(number), end="")
