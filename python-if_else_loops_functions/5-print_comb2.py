f110755d0529a2df374350c969832b4282290fdc
Air-KS <kevinrogeret@gmail.com>
2023-05-29 13:58:36 +0000

    Write a program that prints all numbers in decimal and in hexadecimal

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Kevin R <6265@holbertonstudents.com>
+
+"""Write a program that prints numbers from 00 to 99"""
+for number in range(0, 100):
+    if number == 99:
+        print(f"{number}")
+    else:
+        print("{:02}, ".format(number), end="")
