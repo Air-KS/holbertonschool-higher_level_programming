8b7139c3275fdd6dc7e5122fb2b6d1bf13d6bc41
Air-KS <kevinrogeret@gmail.com>
2023-05-29 12:33:24 +0000

    Write a program that prints the ASCII alphabet except q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,6 +2,5 @@
 # Kevin R <6265@holbertonstudents.com>
 
 for alphabet in range(97, 123):
-    if chr(alphabet) == 'e' or chr(alphabet) == 'q':
-        alphabet += 2
-    print("{}".format(chr(alphabet)), end="")
+    if chr(alphabet) != 'e' and chr(alphabet) != 'q':
+        print("{}".format(chr(alphabet)), end="")
