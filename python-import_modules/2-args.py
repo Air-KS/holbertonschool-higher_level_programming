418e73f53bb5a469c3e830392d507e4d7ffb265c
Air-KS <kevinrogeret@gmail.com>
2023-05-31 13:21:00 +0000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    count = len(sys.argv) - 1
 
-    if count < 0 or count == 0:
-        print("{} arguments".format(count))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("{} argument".format(count))
     elif count == 1:
         print("{} Argument:".format(count))
     else:
