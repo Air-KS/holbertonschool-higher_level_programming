bbd84e61dc814fafe7d733beddae4d61175149f0
Air-KS <kevinrogeret@gmail.com>
2023-05-31 07:55:04 +0000

    prints the result of the addition 1 + 2 = 3

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # add_0.py
-if __name__ =="__main__":
+if __name__ == "__main__":
     """ Affiche la somme de 1 et 2 du fichier add_0.py """
     from add_0 import add
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    resultat = add(a, b)
+    print("{} + {} = {}".format(a, b, resultat))
