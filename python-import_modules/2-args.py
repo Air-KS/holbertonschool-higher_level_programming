8434a9b544a81911cf102263cdaae2dd42e3a474
Air-KS <kevinrogeret@gmail.com>
2023-05-31 09:03:45 +0000

    prints the number of and the list of its arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#
+
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) - 1
+
+    if count < 0 or count == 0:
+        print("Not argument")
+    else:
+        print("{} Argument :".format(count))
+    for index in range(1, count + 1):
+        print(index, ":", sys.argv[index])
+        # print("{}: {}".format(index + 1, sys.argv[index + 1]))
