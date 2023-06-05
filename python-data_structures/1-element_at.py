9a9dc2d786ecdf1be4bb122d420dd466436bc874
Air-KS <kevinrogeret@gmail.com>
2023-06-05 08:56:12 +0000

    Function that retrieves an element from a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
 def element_at(my_list, idx):
-    if 0 <= idx < len(my_list):
-        return my_list[idx]
-    else:
+    if idx < 0:
         return None
+    elif idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
