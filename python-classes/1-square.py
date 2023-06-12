3ef1116ed3a8e8ad24389140c0a05ff3e2d96364
Air-KS <kevinrogeret@gmail.com>
2023-06-12 12:09:39 +0000

    A class Square that defines a square by: (based on 0-square.py)

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 1-Square
+Define a Square class
+"""
+
+
+class Square:
+    """A Square classs"""
+
+    def __init__ (self, size)
+    """
+    Initialize the Square
+
+    Args:
+    size (int): The size of the square"""
+    self.__size = size
+    pass
