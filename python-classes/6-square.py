1480d9aaf9d0b07066aebd1f6b9fe3afb93f159e
Air-KS <kevinrogeret@gmail.com>
2023-06-13 08:06:34 +0000

    class Square that defines a square by: (based on 5-square.py)

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -35,12 +35,13 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not type(value) or len(value) != 2 \
-                or not type(value[0]) is int or not type(value[1]) is int \
-                or value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 position integers")
-        else:
-            self.__position = value
+        if not (
+            isinstance(value, tuple) and
+            len(value) == 2 and
+            all(isinstance(num, int) and num >= 0 for num in value)
+        ):
+            raise TypeError("position must be a tuple of two integers")
+        self.__position = value
 
     def area(self):
         """Return the area of the square"""
