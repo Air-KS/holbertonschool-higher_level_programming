a452bca5653ef7f7de277dd4c331f65a9ae9a1a7
Air-KS <kevinrogeret@gmail.com>
2023-06-14 10:34:17 +0000

    class Rectangle that defines a rectangle by: (based on 2-rectangle.py)

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+based on 1-square.py
+"""
+
+
+class Rectangle:
+    """A Rectangle classs"""
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialize the Rectangle
+
+        Args:
+            width, (int): The width of the new Rectangle
+            height (int): The height of the new Rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter method for the width property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for the height property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """
+
+        """
+        width, height = self.__width, self.__height
+        if width == 0 or height == 0:
+            return ("")
+
+        line = "#" * width
+        rectangle = line
+        for index in range(height - 1):
+            rectangle += "\n" + line
+        return (rectangle)
