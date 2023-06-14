5f544a2d80382966005df94d225b1bf84254c3c1
Air-KS <kevinrogeret@gmail.com>
2023-06-14 13:39:10 +0000

    Function that adds 2 integers.

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """ Return the integer addition a of b """
+
+    typeA, typeB = type(a), type(b)
+    if typeA != int and typeA != float:
+        raise TypeError("a must be an integer")
+    if typeB != int and typeB != float:
+        raise TypeError("b must be an integer")
+
+    return (int(a) + int(b))
