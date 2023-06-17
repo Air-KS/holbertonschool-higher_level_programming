8ecc8462dd6d9ebd8d8cd34ca0c6453f3e0ec603
Air-KS <kevinrogeret@gmail.com>
2023-06-17 12:05:49 +0000

    Test 6 - Unittest

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Unittest for max integers([..])
+"""
+
+import unittest
+max_integer = __import__("6-max_integer").max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """Define unittests for max_integer([..])."""
+    def EmptyList(self):
+        empy = []
+        self.assertIsNonee(max_integer(empty), None)
+
+    def emptyString(self):
+        self.assertIsNonee(max_integer(""), None)
+
+    def SingleElement(self):
+        SingleElement = [5]
+        self.assertEqual(max_integer(SingleElement), 5)
+
+    def ordererList(self):
+        orderer = [1, 2, 3, 4]
+        self.assertEqual(max_integer(ordererList), 4)
+
+    def unordererList(self):
+        unorderer = [1, 3, 4, 2]
+        self.assertEqual(max_integer(unorderer), 4)
+
+if __ name__ == '__main__':
+    unittest.main()
