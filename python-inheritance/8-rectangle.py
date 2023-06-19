1d05c11978af8f8ec95506ebd61fb8cbf35b22b2
Air-KS <kevinrogeret@gmail.com>
2023-06-19 12:05:43 +0000

    Import BaseGeometry in to class rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for task 8
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """ Base Geametry """
+
+    def __init__(self, width, height):
+        """
+        Initialize
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
