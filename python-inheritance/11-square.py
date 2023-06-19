efa340b99c016c3ed22a962748a80735841e5a6d
Air-KS <kevinrogeret@gmail.com>
2023-06-19 12:55:46 +0000

    task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module for task 11
+Module for task 10
 """
 
 Rectangle = __import__('9-rectangle').Rectangle
@@ -16,3 +16,15 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        """
+        calculate area / size
+        """
+        return ("{}".format(self.__size * self.__size))
+
+    def __str__(self):
+        """
+        print area result
+        """
+        return ("[Square] {}/{}".format(self.__size, self.__size))
