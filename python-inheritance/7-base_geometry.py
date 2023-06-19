eddb25c0e59e217d769ef979912108c6c071a764
Air-KS <kevinrogeret@gmail.com>
2023-06-19 10:14:33 +0000

    Base Geometry - Add def integer_valisator

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for task 7
+"""
+
+
+class BaseGeometry:
+    """ Base Geametry """
+
+    def area(self):
+        """Method not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate value by type and value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
