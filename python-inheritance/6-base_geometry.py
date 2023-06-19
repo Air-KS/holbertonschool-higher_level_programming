c694d98411b894c8b27b95a1fca2689498f0ee88
Air-KS <kevinrogeret@gmail.com>
2023-06-19 10:03:22 +0000

    Base geometry - Add def area()

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for task 6
+"""
+
+
+class BaseGeometry:
+    """ Base Geametry """
+
+    def area(self):
+        """Method not implemented"""
+        raise Exception("area() is not implemented")
