0c648876c10e9b9ca42ffd645b283df13b996b7e
Air-KS <kevinrogeret@gmail.com>
2023-06-20 13:28:49 +0000

    Function that appends a string at the end of a text file (UTF8) and returns the number of characters added

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,6 +6,6 @@ Module for task 2
 
 def append_write(filename="", text=""):
     """ Append text to a file """
-    with open(filename, "a", encoding="utf-8") as filez:
+    with open(filename, "a", encoding="utf-8") as file:
         file.write(text)
         return len(text)
