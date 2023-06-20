da17b7e2544a458d692b0856f20cbc51c552de5f
Air-KS <kevinrogeret@gmail.com>
2023-06-20 13:28:38 +0000

    voila

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,6 +6,6 @@ Module for task 2
 
 def append_write(filename="", text=""):
     """ Append text to a file """
-    with open(filename, "a", encoding="utf-8") as file:
+    with open(filename, "a", encoding="utf-8") as filez:
         file.write(text)
         return len(text)
