5a7e32114f1d30d48bcac3ea758d3460319efa65
Air-KS <kevinrogeret@gmail.com>
2023-06-23 09:26:13 +0000

    Move commentary

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -13,11 +13,13 @@ class Student():
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """ Convert the given attributes """
         if attrs is None:
             return self.__dict__
         else:
