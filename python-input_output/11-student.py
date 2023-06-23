b328ae764292518be677736c926f8675c4bc4a36
Air-KS <kevinrogeret@gmail.com>
2023-06-23 09:22:58 +0000

    Move commentary

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -5,7 +5,6 @@ Module for task 11
 
 
 class Student():
-    def __init__(self, first_name, last_name, age):
         """ Initialize a new Student.
 
         Args:
@@ -13,6 +12,8 @@ class Student():
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """
+    def __init__(self, first_name, last_name, age):
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
