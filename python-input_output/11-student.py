d7e04fd9ea2662e6180072c3e2ecbdc01241270d
Air-KS <kevinrogeret@gmail.com>
2023-06-23 09:27:52 +0000

    refonte

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-"""
-Module for task 11
-"""
+'''
+    This module contains a class Student
+'''
 
 
-class Student():
+class Student:
+    '''Initialization of attributes
+
+            Args:
+                first_name(str): first attribute
+                last_name(str): second attribute
+                age(int): third attribute
+    '''
+
     def __init__(self, first_name, last_name, age):
-        """ Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ Convert the given attributes """
-        if attrs is None:
-            return self.__dict__
-        else:
-            result = {}
-            for attr in attrs:
-                if hasattr(self, attr):
-                    result[attr] = getattr(self, attr)
-            return result
+        """
+        Return a dictionary
+        """
+
+        if isinstance(attrs, list) and all(isinstance(attr, str)
+                                           for attr in attrs):
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+
+        return (self.__dict__)
 
     def reload_from_json(self, json):
-        """ Transfer all attributes of json to self """
-        for key, value in json.iteritems():
+        """Replaces attributes of instance"""
+        for key, value in json.items():
             setattr(self, key, value)
