19781760b8856d211f828e434b966f380b4d9455
Air-KS <kevinrogeret@gmail.com>
2023-06-27 09:48:14 +0000

    Add function dict

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -57,3 +57,13 @@ class Square(Rectangle):
                 self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """ Return dictionary representation """
+        dictionary = {}
+        dictionary["id"] = self.id
+        dictionary["width"] = self.width
+        dictionary["height"] = self.height
+        dictionary["x"] = self.x
+        dictionary["y"] = self.y
+        return (dictionary)
