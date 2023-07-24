4d671351bca130c4bbcc083a5d3eef2f001f16f2
Air-KS <kevinrogeret@gmail.com>
2023-07-24 11:25:02 +0000

    Create a new table

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- Create a new table
+CREATETE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUE (1, 'john', 10), (2, 'alex', 3), (3, 'bob', 14), (4, 'George', 8);
