540c04064d70135f7a63c7a996dfcaa64e606b2d
Air-KS <kevinrogeret@gmail.com>
2023-07-24 12:06:18 +0000

    Lists the number of records with the same score in the table

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
---  Lists the number of records with the same score in the table
+-- Lists the number of records with the same score in the table
 SELECT COUNT(score) AS 'number' FROM second_table GROUP BY score ORDER BY number DESC
