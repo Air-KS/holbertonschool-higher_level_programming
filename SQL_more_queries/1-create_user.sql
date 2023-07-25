24619a2886bc203e7eb1189f4424bbe0ae40a7e3
Air-KS <kevinrogeret@gmail.com>
2023-07-25 10:42:06 +0200

    Creates the database, Creates user_0d_2 and add privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Creates a user user_0d_1@localhost
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges to user_0d_1@localhost
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
