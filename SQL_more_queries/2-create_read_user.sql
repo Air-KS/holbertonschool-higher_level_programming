4fe8f931398a60c04a79db0b98190c1984157152
Air-KS <kevinrogeret@gmail.com>
2023-07-25 09:58:18 +0200

    Creates the database, Creates user_0d_2 and add privileges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+﻿-- Creates the database hbtn_2
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+    
+-- Creates user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant privileges to user
+GRANT SELECT ON 'hbtn_0d_2'.*
+      TO 'user_0d_2'@'localhost';
