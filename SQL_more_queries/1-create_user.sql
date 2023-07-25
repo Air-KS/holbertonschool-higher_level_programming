0ba0a3eda61d6bac75bdc8c5e3b78c5a23f2f8d2
Air-KS <kevinrogeret@gmail.com>
2023-07-25 09:27:00 +0200

    Creates a user add all previleges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,10 @@
--- creates the MySQL server user user_0d_1
+-- Grant previlges in MySQL server
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+-- Creates a user user_0d_1@localhost
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+    IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all previleges to user_0d_1@localhost
+GRANT ALL PRIVILEGES
+    ON *.*
+    TO USER_0d_1@localhost;
