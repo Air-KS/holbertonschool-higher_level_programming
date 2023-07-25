5342bf979924f2fbdd7d38810ce1a0bf1edde0a5
Air-KS <kevinrogeret@gmail.com>
2023-07-25 16:04:22 +0200

    List join Dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,10 @@
 ﻿-- MySQL server
 -- List join Dexter
-SELECT tv_genres.name 
+SELECT tv_genres.name
 FROM tv_genres
-    LEFT JOIN tv_shows_genres
-        ON tv_genres.id = tv_shows_genre.genre.id
-    LEFT JOIN tv_shows
-        ON tv_shows_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Dexter" 
-ORDER BY tv_genres.name ASC ;
+         INNER JOIN tv_show_genres
+                    ON tv_genres.id = tv_show_genres.genre_id
+         INNER JOIN tv_shows
+                    ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
