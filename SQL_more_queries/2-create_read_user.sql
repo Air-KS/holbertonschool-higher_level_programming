e2deb116258a4c19f09fec586a4658575547b3a2
Air-KS <kevinrogeret@gmail.com>
2023-07-25 16:20:32 +0200

    Show privileges, create users with diff privileges

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,7 @@
 ﻿-- Creates database and user in MySQL server
-
 -- Creates the database hbtn_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-    
--- Creates user_0d_2
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant privileges to user
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
