884c036a5e7df44809d8dd332d4d6fa7f4895d44
Air-KS <kevinrogeret@gmail.com>
2023-07-25 09:28:55 +0200

    Creates a user add all previleges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,10 @@
 -- Grant previlges in MySQL server
 
 -- Creates a user user_0d_1@localhost
-CREATE USER IF NOT EXISTS user_0d_1@localhost
-    IDENTIFIED BY user_0d_1_pwd;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all previleges to user_0d_1@localhost
 GRANT ALL PRIVILEGES
     ON *.*
-    TO USER_0d_1@localhost;
+    TO 'USER_0d_1'@'localhost';
