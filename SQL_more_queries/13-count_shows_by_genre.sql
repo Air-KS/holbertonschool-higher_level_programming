f7e801ab6948c029b756f7306494fffef503520f
Air-KS <kevinrogeret@gmail.com>
2023-07-25 15:04:02 +0200

    List all genre from tv_shows with number

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+﻿-- MySQL server
+-- List all genre from tv_shows with number
+SELECT tv_genre.name AS genre,
+       COUNT(*) AS number_of_shows
+FROM tv_genre 
+    JOIN tv_show_genres 
+        ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show.name
+ORDER BY number_of_shows DESC;
+
