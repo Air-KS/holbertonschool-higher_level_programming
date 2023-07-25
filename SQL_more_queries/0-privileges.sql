14fd741b25d68bacaf18b73d78c43ee633f9a5e1
Air-KS <kevinrogeret@gmail.com>
2023-07-25 09:13:21 +0200

    List all privileges of the user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 ﻿-- List all privileges of the user_0d_1 and user_0d_2
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
