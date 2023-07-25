85f246a1735f96eb05b44edd8b754d3555dc1b86
Air-KS <kevinrogeret@gmail.com>
2023-07-25 14:33:35 +0200

    Lists all shows contained in hbtn_0d_tvshows without a genre linked

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
-﻿-- Lists all shows contained in hbtn_0d_tvshows without a genre linked
-
+﻿-- MySQL server
+-- Lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
          LEFT JOIN tv_show_genres
