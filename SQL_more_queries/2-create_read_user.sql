ad58892c0f9ee8370ea836f400a9bb16e66434f7
Air-KS <kevinrogeret@gmail.com>
2023-07-25 10:04:43 +0200

    Creates the database, Creates user_0d_2 and add privileges

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,12 @@
 ﻿-- Creates database and user in MySQL server
 
 -- Creates the database hbtn_2
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
     
 -- Creates user_0d_2
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant privileges to user
-GRANT SELECT ON 'hbtn_0d_2'.*
+GRANT SELECT ON hbtn_0d_2.*
       TO 'user_0d_2'@'localhost';
