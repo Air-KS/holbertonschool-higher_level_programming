3018511e378f7bf4a12eae30965a18901d4c2c14
Air-KS <kevinrogeret@gmail.com>
2023-07-25 14:31:42 +0200

    -- Lists all cities contained in the database hbtn_0d_usa

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
-﻿-- Lists all cities contained in the database hbtn_0d_usa
+﻿-- MySQL server
+-- Lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name 
 FROM cities 
 JOIN states 
