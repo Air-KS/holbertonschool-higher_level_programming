eba69268795bd8a58d9eeeb482e0467f8fae51b1
Air-KS <kevinrogeret@gmail.com>
2023-07-25 14:09:34 +0200

    -- Lists all cities contained in the database hbtn_0d_usa

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 SELECT cities.id, cities.name, states.name 
     FROM cities 
     JOIN states 
-    WHERE cities.state_id = states.id 
+    WHERE states on cities.state_id = states.id 
 ORDER BY cities.id ASC;
