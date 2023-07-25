5378759c935007dc3ffcf410496ebd5292ecd0a2
Air-KS <kevinrogeret@gmail.com>
2023-07-25 11:25:12 +0200

    List all the cities of california tha can be found

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+﻿-- List all the cities of california tha can be found
+-- In the database hbtn_0d_usa
+SELECT id, name FROM cities WHERE states_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY id;
