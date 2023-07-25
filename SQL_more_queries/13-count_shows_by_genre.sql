34e86bbd56f5959445007f845121c6d9f39916dc
Air-KS <kevinrogeret@gmail.com>
2023-07-25 15:14:28 +0200

    List all genre from tv_shows with number

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,7 @@
 SELECT name AS genre,
        COUNT(genre_id) AS number_of_shows
 FROM tv_genre 
-    INNER JOIN tv_show_genres 
+    INNER JOIN 'tv_show_genres' 
         ON id = genres.id
-GROUP BY genre_id
+GROUP BY 'genre_id'
 ORDER BY number_of_shows DESC;
