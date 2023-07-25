8b87405b9298f57ae9d0a5a1745d712b27b7e316
Air-KS <kevinrogeret@gmail.com>
2023-07-25 11:10:45 +0200

    Create database hbtn_0d_usa

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 ﻿-- Create database hbtn_0d_usa
--- 
+-- Table states (In the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
                             (id INT UNIQUE AUTO_INCRETEMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
