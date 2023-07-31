20427f56ad572ff783970b0a9ef6945f0e3fcb90
Air-KS <kevinrogeret@gmail.com>
2023-07-31 17:00:01 +0200

    Task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """Lists all states  starting with ‘N’ from the database hbtn_0e_0_usa"""
-import mysql.connector
+import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
     # connect to database
-    db = mysql.connector.connect(host="localhost",
-                                 port="3306",
-                                 user=argv[1],
-                                 password=argv[2],
-                                 db=argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         port="3306",
+                         user=argv[1],
+                         password=argv[2],
+                         db=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states \
