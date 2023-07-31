358333d11c1b165cc1d4e80ae0deaa71b523163f
Air-KS <kevinrogeret@gmail.com>
2023-07-31 16:57:36 +0200

    Task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Lists all states  starting with ‘N’ from the database hbtn_0e_0_usa"""
+import mysql.connector
+from sys import argv
+
+
+if __name__ == "__main__":
+    # connect to database
+    db = mysql.connector.connect(host="localhost",
+                                 port="3306",
+                                 user=argv[1],
+                                 password=argv[2],
+                                 db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                WHERE name=%s ORDER BY id ASC", (argv[4],))
+
+    all = cur.fetchall()
+    for row in all:
+        print(row)
+    cur.close()
+    db.close()
