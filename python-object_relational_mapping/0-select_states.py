4f23ad5b072a534f90cf8bf8de022d601f757df4
Air-KS <kevinrogeret@gmail.com>
2023-07-31 14:41:16 +0200

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,17 +4,17 @@ return all table values (table 'states')
 parameters given to script: username, password, database
 """
 
-import MySQLdb
+import mysql.connector
 from sys import argv
 
 if __name__ == "__main__":
 
     # connect to database
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+    db = mysql.connector.connect(host="localhost",
+                                 port=3306,
+                                 user=argv[1],
+                                 passwd=argv[2],
+                                 db=argv[3])
 
     # create cursor to exec queries using SQL
     cursor = db.cursor()
