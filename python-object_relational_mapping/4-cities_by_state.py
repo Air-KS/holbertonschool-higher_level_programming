7942041f2cf6591d600279e7c4d6426769f2da6e
Air-KS <kevinrogeret@gmail.com>
2023-07-31 17:55:07 +0200

    Task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -2,16 +2,16 @@
 """ SQL injection to delete all record of a table"""
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
 
     # connect to database
     db = MySQLdb.connect(
         host="localhost", port="3306",
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
 
     # Create cursor to execute using SQL; join two tables for all info
     cur = db.cursor()
