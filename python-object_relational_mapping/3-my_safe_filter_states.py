18f50b273bb6c829b65c8e81a5f23afd72c37b2f
Air-KS <kevinrogeret@gmail.com>
2023-07-31 17:14:58 +0200

    Task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-
+""" SQL injection to delete all record of a table"""
 
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
+    
     # connect to database
     db = MySQLdb.connect(host="localhost", port="3306",
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
 
+    # Cursor
     cur = db.cursor()
 
     # Exécute une requête SQL
-    cur.execute("SELECT * FROM states \
-                WHERE name=%s \
-                ORDER BY id ASC", (argv[4],))
+    cur.execute(
+        "SELECT * FROM states \
+        WHERE name=%s \
+        ORDER BY id ASC", (argv[4],)
+    )
 
-    all = cur.fetchall()
-    for row in all:
+    states = cur.fetchall()
+    for row in states:
         print(row)
+
     cur.close()
     db.close()
