27b60a93cae3a88e83d5a51dd0db357232369be3
Air-KS <kevinrogeret@gmail.com>
2023-07-31 09:40:38 +0000

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,30 +4,22 @@ return all table values (table 'states')
 parameters given to script: username, password, database
 """
 
-import sys
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    # Se connecter à la base de données MySQL
-    db = MySQLdb.connect(host='127.0.0.1', port=3306, user=username, passwd=password, db=database)
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
 
-    # Créer un curseur pour exécuter des requêtes SQL
+    # create cursor to exec queries using SQL
     cursor = db.cursor()
-
-    # Exécuter une requête SELECT sur la table des états
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Récupérer et afficher les résultats
-    results = cursor.fetchall()
-    for row in results:
-        state_id = row[0]
-        state_name = row[1]
-        print("{}: {}".format(state_id, state_name))
-
-    # Fermer le curseur et la connexion à la base de données
+    for row in cursor.fetchall():
+        print(row)
     cursor.close()
     db.close()
