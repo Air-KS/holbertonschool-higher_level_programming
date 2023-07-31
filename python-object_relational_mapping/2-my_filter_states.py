7f37e089d743e4ad52d23e9407e0637c58ca6c2d
Air-KS <kevinrogeret@gmail.com>
2023-07-31 16:40:23 +0200

    Task 1

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Lists all states  starting with ‘N’ from the database hbtn_0e_0_usa"""
-import mysql.connector
+import MySQLdb
 from sys import argv
 
 
 if __name__ == "__main__":
     # connect to database
-    db = mysql.connector.connect(host="localhost",
+    db = MySQLdb.connect(host="localhost",
                                  port="3306",
                                  user=argv[1],
                                  password=argv[2],
