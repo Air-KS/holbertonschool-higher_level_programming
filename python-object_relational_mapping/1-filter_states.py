1bd636d7f637cd88e247fc1e0683fa9dc9cd873f
Air-KS <kevinrogeret@gmail.com>
2023-08-03 09:03:07 +0000

    Coorect Checker

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
-"""
-Lists all states  starting with ‘N’
-from the database hbtn_0e_0_usa
-"""
-from sys import argv
+"""Lists all states  starting with 'N' from the database hbtn_0e_0_usa"""
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    # connect to database
+if __name__ == '__main__':
     db = MySQLdb.connect(
-        host="localhost",
-        port="3306",
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
+        host="localhost", port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
 
-    # Cursor
     cur = db.cursor()
 
-    # Exécute une requête SQL
     cur.execute(
-        "SELECT * FROM states \
-         WHERE name \
-         LIKE BINARY 'N%' ORDER BY states.id ASC"
-    )
+            "SELECT * FROM states \
+            WHERE name LIKE binary 'N%' \
+            ORDER BY states.id ASC;")
 
-    all = cur.fetchall()
-    for row in all:
-        print(row)
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
 
     cur.close()
     db.close()
